f661480fe6066cac88256e34c0e45cae6bcfb5ce
abxl1 <etho_818@naver.com>
2024-10-25 21:09:04 +0900

    [FIX] (common) GlobalExceptionHandler Exception.class 하위로 코드 이동 / ErrorCode 패키지 이동 import 수정

--- src/main/java/com/example/lastproject/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/lastproject/common/GlobalExceptionHandler.java
@@ -13,19 +13,6 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     /**
-     * 포괄 예외 처리 <br />
-     * -
-     * 이 핸들러는 세부 예외 처리보다 낮은 우선 순위로 처리되는 예외 처리 핸들러입니다. <br />
-     * 주로 예기치 않은 에러 상황에 해당 에러가 반환됩니다.
-     * @param ex 오류 상태
-     * @return 오류 응답을 포함한 ResponseEntity 객체
-     */
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
-        return getErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
-    }
-
-    /**
      * 비밀번호 오류 Exception
      * @param ex 오류 상태, 메시지
      * @return 해당 내용이 담긴 에러 객체
@@ -33,12 +20,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<Map<String, Object>> handlePasswordMismatchException(CustomException ex) {
         return getErrorResponse(ex.getErrorCode().getStatus(), ex.getMessage());
-    }
-
-    // 핸들러 구현 수정 필요
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return getErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
     /**
@@ -54,5 +35,18 @@ public class GlobalExceptionHandler {
         errorResponse.put("message", message);
 
         return new ResponseEntity<>(errorResponse, status);
+    }
+
+    /**
+     * 포괄 예외 처리 <br />
+     * -
+     * 이 핸들러는 세부 예외 처리보다 낮은 우선 순위로 처리되는 예외 처리 핸들러입니다. <br />
+     * 주로 예기치 않은 에러 상황에 해당 에러가 반환됩니다.
+     * @param ex 오류 상태
+     * @return 오류 응답을 포함한 ResponseEntity 객체
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleException(Exception ex) {
+        return getErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 }
