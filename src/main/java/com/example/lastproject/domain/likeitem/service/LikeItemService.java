4b556584bb4bdac5e157f17eec645b11ced63180
SpringStudy <dnjsdyd78@gmail.com>
2024-10-25 21:29:50 +0900

    [FEAT] (LikeItem,item) 즐겨찾기 품목 등록 기능 구현, 품목 삭제 기능 구현 [ADD] QueryDslConfig 생성/ MarketController, LikeItemController, ItemController 주석추가 [FIX] Market 필드명 변경 (storeName => marketName)

--- src/main/java/com/example/lastproject/domain/likeitem/service/LikeItemService.java
+++ src/main/java/com/example/lastproject/domain/likeitem/service/LikeItemService.java
@@ -1,15 +1,41 @@
 package com.example.lastproject.domain.likeitem.service;
 
+import com.example.lastproject.common.CustomException;
+import com.example.lastproject.common.ErrorCode;
+import com.example.lastproject.domain.auth.entity.AuthUser;
+import com.example.lastproject.domain.item.entity.Item;
+import com.example.lastproject.domain.item.service.ItemService;
+import com.example.lastproject.domain.likeitem.dto.response.LikeItemResponseDto;
+import com.example.lastproject.domain.likeitem.entity.LikeItem;
 import com.example.lastproject.domain.likeitem.repository.LikeItemRepository;
+import com.example.lastproject.domain.user.entity.User;
+import com.example.lastproject.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class LikeItemService {
     private final LikeItemRepository likeItemRepository;
+    private final UserRepository userRepository;
+    private final ItemService itemService;
 
-    public Object getItems(String keyword) {
+    @Transactional
+    public void bookmarkItem(AuthUser authUser, Long itemId) {
+        User user = userRepository.findById(authUser.getUserId()).orElseThrow(
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
+        );
+
+        Item item = itemService.validateEntity(itemId);
+
+        LikeItem likeItem = new LikeItem(user,item);
+
+        likeItemRepository.save(likeItem);
+    }
+
+    public LikeItemResponseDto getLikeItems(AuthUser authUser) {
         return null;
     }
 }
--- src/main/java/com/example/lastproject/domain/market/marketcontroller/MarketController.java
+++ src/main/java/com/example/lastproject/domain/market/marketcontroller/MarketController.java
@@ -13,11 +13,21 @@ public class MarketController {
 
     private final MarketService marketService;
 
+    /**
+     * 마켓 등록
+     * @param requestDto 등록할 마켓 객체데이터
+     * @return 성공메시지
+     */
     @PostMapping
     public ResponseEntity<String> saveMarket(@RequestBody MarketRequestDto requestDto){
         return ResponseEntity.ok(marketService.saveMarket(requestDto));
     }
 
+    /**
+     * 마켓 삭제
+     * @param marketId 삭제할 마켓 아이디
+     * @return 성공 메시지
+     */
     @DeleteMapping("/{marketId}")
     public ResponseEntity<String> deleteMarket(@PathVariable Long marketId){
         return ResponseEntity.ok(marketService.deleteMarket(marketId));
