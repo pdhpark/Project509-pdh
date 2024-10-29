a97b47a7c7a4c6f0eb043878318b9839230c4180
dlrkdus12 <lsd975@naver.com>
2024-10-29 15:37:27 +0900

    [FIX] (notification) 컨벤션률에 따라 수정

--- src/main/java/com/example/lastproject/domain/notification/dto/response/NotificationListResponseDto.java
+++ src/main/java/com/example/lastproject/domain/notification/dto/response/NotificationListResponseDto.java
@@ -3,7 +3,6 @@ package com.example.lastproject.domain.notification.dto.response;
 import com.example.lastproject.domain.notification.entity.Notification;
 import lombok.Builder;
 import lombok.Getter;
-
 import java.util.List;
 
 @Getter
