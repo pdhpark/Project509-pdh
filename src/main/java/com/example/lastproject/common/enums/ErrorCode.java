6c31cca9f06661421f01401ccff9ff4038355523
abxl1 <etho_818@naver.com>
2024-11-04 16:50:52 +0900

    [FIX] (common) 에러 코드 추가, 의존성 버전 수정

--- src/main/java/com/example/lastproject/common/enums/ErrorCode.java
+++ src/main/java/com/example/lastproject/common/enums/ErrorCode.java
@@ -28,10 +28,12 @@ public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 조회에 실패했습니다."),
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "검증에 실패했습니다."),
     EXIST_EMAIL(HttpStatus.BAD_REQUEST, "존재하는 이메일입니다."),
+    EXIST_NICKNAME(HttpStatus.BAD_REQUEST, "존재하는 닉네임입니다."),
     SIGNUP_ERROR(HttpStatus.BAD_REQUEST, "가입에 실패했습니다."),
     SIGNIN_ERROR(HttpStatus.BAD_REQUEST, "로그인에 실패했습니다."),
     WITHDRAWAL_ERROR(HttpStatus.BAD_REQUEST, "탈퇴에 실패했습니다."),
     NO_CONTENTS(HttpStatus.BAD_REQUEST, "변경된 정보가 없습니다."),
+    ENCODING_FAILED(HttpStatus.BAD_REQUEST, "인코딩에 실패했습니다."),
 
     // UserRole ErrorCode
     USER_ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 권한 조회에 실패했습니다."),
