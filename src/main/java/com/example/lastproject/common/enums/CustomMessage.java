9f80135d496838b2d311a68f59b87e4955d158be
abxl1 <etho_818@naver.com>
2024-11-13 16:51:10 +0900

    [FEAT] (common) 성공 메시지 추가

--- src/main/java/com/example/lastproject/common/enums/CustomMessage.java
+++ src/main/java/com/example/lastproject/common/enums/CustomMessage.java
@@ -10,9 +10,9 @@ public enum CustomMessage {
     SIGNUP_SUCCESS(HttpStatus.OK, "가입이 완료되었습니다."),
     SIGNIN_SUCCESS(HttpStatus.OK, "로그인이 완료되었습니다."),
     WITHDRAWAL_SUCCESS(HttpStatus.OK, "님의 탈퇴가 완료되었습니다."),
-    USER_FOUND(HttpStatus.OK, "님이 조회되었습니다."),
-    CHANGE_PW_SUCCESS(HttpStatus.OK, "님의 비밀번호 변경이 완료되었습니다."),
-    CHANGE_DETAIL_SUCCESS(HttpStatus.OK, "님의 정보 변경이 완료되었습니다."),
+    USER_FOUND(HttpStatus.OK, "조회되었습니다."),
+    CHANGE_PW_SUCCESS(HttpStatus.OK, "비밀번호 변경이 완료되었습니다."),
+    CHANGE_DETAIL_SUCCESS(HttpStatus.OK, "정보 변경이 완료되었습니다."),
 
     // Penalty Success
     PENALTY_SEND_SUCCESS(HttpStatus.OK, "해당 유저에게 페널티를 부과했습니다."),
