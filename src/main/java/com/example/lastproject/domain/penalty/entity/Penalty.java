65bb702add709db2e71666aa9d60d3486debad3b
abxl1 <etho_818@naver.com>
2024-11-13 16:52:49 +0900

    [FIX] (Penalty) PenaltyStatus ENUM 제거

--- src/main/java/com/example/lastproject/domain/penalty/entity/Penalty.java
+++ src/main/java/com/example/lastproject/domain/penalty/entity/Penalty.java
@@ -2,7 +2,6 @@ package com.example.lastproject.domain.penalty.entity;
 
 import com.example.lastproject.common.Timestamped;
 import com.example.lastproject.domain.party.entity.Party;
-import com.example.lastproject.domain.penalty.enums.PenaltyStatus;
 import com.example.lastproject.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -29,9 +28,6 @@ public class Penalty extends Timestamped {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User userId;
-
-    @Enumerated(EnumType.STRING)
-    private PenaltyStatus status = PenaltyStatus.SEARCHABLE; // 3개월이 지나면 조회 불가
 
     public Penalty(Party partyId, User userId) {
         this.partyId = partyId;
